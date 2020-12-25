319442fc86fd36408c4d44d5efc23630be86b155
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2020-12-25 19:31:16 +0800

    Update config.php

--- .data/config.php
+++ .data/config.php
@@ -1,5 +1,17 @@
 <?php $configs = '
 {
-
+    "admin": "7749.1",
+    "adminloginpage": "sakura",
+    "autoJumpFirstDisk": "1",
+    "background": "aHR0cHM6Ly9hcGkuaXhpYW93YWkuY24vYXBpL2FwaS5waHA",
+    "backgroundm": "aHR0cHM6Ly9hY2cueWFud3ouY24vYXBpLnBocA",
+    "customCss": "PHN0eWxlPi5jaGFuZ2VsYW5ndWFnZXtkaXNwbGF5Om5vbmV9PC9zdHlsZT4",
+    "customTheme": "aHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL2tpengvb25lbW9lLXRoZW1lL29uZW1vZS5odG1s",
+    "disableChangeTheme": "1",
+    "disableShowThumb": "0",
+    "disktag": "Sakura|Mine",
+    "hideFunctionalityFile": "1",
+    "sitename": "U2FrdXJhIENsb3Vk",
+    "timezone": "8"
 }
 ';
